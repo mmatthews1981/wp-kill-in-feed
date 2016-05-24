eb577197648d83fc850025fe9d0372511531a287
M. Matthews <mmatthews1981@users.noreply.github.com>
2016-05-23 23:32:14 -0400

    Update kill-in-feed.php

--- kill-in-feed.php
+++ kill-in-feed.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Shortcode to remove or add post content from feed
-Plugin URI:
+Plugin Name: WP Kill-In-Feed
+Plugin URI: http://github.com/mmatthews1981/wp-kill-in-feed
 Description: This plugin gives you two new shortcodes, [killinfeed] and [addtofeed], that will prevent whatever it's wrapped around from being included in your site's RSS feed, or add exclusive feed-only content, in that order. It's especially useful for wrapping around multimedia shortcodes that are adding bits and pieces of the media player into your feed.
 Author: Meredith Matthews
 Version: 1
-Author URI: http://braindouche.com/
+Author URI: http://github.com/mmatthews1981
 */
 
 function bd_shortcode_killinfeed( $atts, $content ) {
